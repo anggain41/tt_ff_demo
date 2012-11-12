038e5c0f15aa36afa8e18afb2f553bf945461424
dariel <annas_ramadhani@yahoo.co.id>
2012-11-12 10:27:10 +0700

    initial commit for first time

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+TtFfDemo::Application.config.secret_token = 'a73ffccbd8d797b96678934737c846942580648cc72b66b6e4e1f8667787ad8f29f33f87d4ecf7c4247999c17857ae3adaaaec1b466cd26aadaf61dbf9a6130b'
